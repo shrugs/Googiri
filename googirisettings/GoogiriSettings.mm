e4971015ee12f2deb79f4b94b4c20b2631490ad7
Matt Condon <MattCMultimedia@gmail.com>
2014-03-23 19:23:55 -0500

    fixed up settings

--- googirisettings/GoogiriSettings.mm
+++ googirisettings/GoogiriSettings.mm
@@ -7,9 +7,9 @@ static CFNotificationCenterRef darwinNotifyCenter = CFNotificationCenterGetDarwi
 }
 
 - (void)twitter:(id)arg;
-- (void)openAlexWrightPortfolio:(id)arg;
-- (void)setPreferenceValue:(id)value specifier:(id)specifier;
-- (void)openJustAddDesignPortfolio:(id)arg;
+// - (void)openAlexWrightPortfolio:(id)arg;
+// - (void)setPreferenceValue:(id)value specifier:(id)specifier;
+// - (void)openJustAddDesignPortfolio:(id)arg;
 @end
 
 @implementation GoogiriSettingsListController
@@ -24,30 +24,6 @@ static CFNotificationCenterRef darwinNotifyCenter = CFNotificationCenterGetDarwi
     [super setPreferenceValue:value specifier:specifier];
 
     NSString *notification = [specifier propertyForKey:@"postNotification"];
-    // if ([notification isEqualToString:@"com.mattcmultimedia.googirisettings/useAltIcon"]) {
-    //     //change icons here.
-    //     NSMutableDictionary *entry = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/Library/PreferenceLoader/Preferences/GoogiriSettings.plist"];
-    //     //load entry dict
-    //     NSLog(@"entry: %@", entry);
-    //     if ([(NSString *)[[entry objectForKey:@"entry"] objectForKey:@"icon"] isEqualToString:@"googiri.png"]) {
-    //         //if is original icon, switch to alt
-    //         //[entry removeObjectForKey:@"icon"];
-    //         [[entry objectForKey:@"entry"] removeObjectForKey:@"icon"];
-    //         [[entry objectForKey:@"entry"] setObject:@"altGoogiri.png" forKey:@"icon"];
-    //     } else {
-    //         //is alt, switch to orginal
-    //         [[entry objectForKey:@"entry"] removeObjectForKey:@"icon"];
-    //         [[entry objectForKey:@"entry"] setObject:@"googiri.png" forKey:@"icon"];
-    //     }
-    //     NSLog(@"new Dictionary: %@", entry);
-    //     NSDictionary *newEntry = [[NSDictionary alloc] initWithDictionary:entry];
-    //     BOOL succeed = [newEntry writeToFile:@"/Library/PreferenceLoader/Preferences/GoogiriSettings.plist" atomically:YES];
-    //     NSLog(@"succeeded? %i", succeed);
-    //     [entry release];
-    //     [newEntry release];
-    //     //if "alt in the icon name, change to norm, else, change to alt"
-    //     //save back to disk
-    // }
     if(notification) {
         CFNotificationCenterPostNotification(darwinNotifyCenter, (CFStringRef)notification, NULL, NULL, true);
     }
@@ -65,28 +41,28 @@ static CFNotificationCenterRef darwinNotifyCenter = CFNotificationCenterGetDarwi
 
 }
 
-- (void)openAlexWrightPortfolio:(id)arg {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://cargocollective.com/rednaxela1111"]];
-}
+// - (void)openAlexWrightPortfolio:(id)arg {
+//     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://cargocollective.com/rednaxela1111"]];
+// }
 
-- (void)openDemoVideo:(id)arg {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.youtube.com/watch?v=7vuY2kPMQZs"]];
-}
+// - (void)openDemoVideo:(id)arg {
+//     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.youtube.com/watch?v=7vuY2kPMQZs"]];
+// }
 
-- (void)openJustAddDesignPortfolio:(id)arg {
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://justadddesign.net/"]];
-}
+// - (void)openJustAddDesignPortfolio:(id)arg {
+//     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://justadddesign.net/"]];
+// }
 
 @end
 
-//googiriSpecificSystemCommands
-@interface GoogiriSpecificSystemCommands: PSListController {
+//GoogiriExtraNames
+@interface GoogiriExtraNames: PSListController {
 }
 - (void)setPreferenceValue:(id)value specifier:(id)specifier;
 
 @end
 
-@implementation GoogiriSpecificSystemCommands
+@implementation GoogiriExtraNames
 - (void)setPreferenceValue:(id)value specifier:(id)specifier {
     [super setPreferenceValue:value specifier:specifier];
 
